88c64cb180c341264bc0ffd55e7a8b63df8e6904
infie007 <yuxuan.chen.18@ucl.ac.uk>
2021-11-13 20:34:44 +0800

    handle hello world

---
+++ handler/hello_world.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HandleHelloWorld(c *gin.Context) {
+	token := c.DefaultQuery("token", "0")
+	timestamp := c.DefaultQuery("timestamp", "0")
+	nonce := c.DefaultQuery("timestamp", "0")
+	echostr := c.DefaultQuery("echostr", "0")
+	log.Printf("change: %v, token: %v, nonce: %v, echostr: %v", token, timestamp, nonce, echostr)
+
+	c.String(http.StatusOK, echostr)
+}
--- router.go
+++ router.go
@@ -10,6 +10,7 @@ func BuildRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/change_score", handler.HandleChangeScore)
+	router.GET("/hello_world", handler.HandleHelloWorld)
 
 	return router
 }
