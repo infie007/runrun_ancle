674914875900e90b1b4f5f306e29af1b5ad48d87
infie007 <yuxuan.chen.18@ucl.ac.uk>
2021-11-13 21:29:04 +0800

    handle command

--- handler/command.go
+++ handler/command.go
@@ -4,7 +4,6 @@ import (
 	"encoding/xml"
 	"fmt"
 	"log"
-	"net/http"
 	"runrun_uncle/dal/redis"
 	"runrun_uncle/model"
 	"runrun_uncle/tools"
@@ -28,13 +27,34 @@ func HandleCommand(c *gin.Context) {
 	if msg.Content == "分数" {
 		currentScore, err := redis.GetScore()
 		if err != nil {
-			c.String(http.StatusOK, "接口错误")
+			tools.NewReply(c, msg, "接口错误，快喊陈🐷来修bug！")
 			return
 		}
 		log.Printf("current: %v", currentScore)
-		currentScoreString := fmt.Sprintf("当前陈🐷在陈🐷🐷心目中的分数是%.1f", currentScore)
+		currentScoreString := fmt.Sprintf("当前陈🐷在小香🐷心目中的分数是%.1f，要继续努力哦～", currentScore)
 		tools.NewReply(c, msg, currentScoreString)
+		return
+	} else if change := tools.ParseFloat(msg.Content); change != 0 {
+		currentScore, err := redis.GetScore()
+		if err != nil {
+			tools.NewReply(c, msg, "接口错误，快喊陈🐷来修bug！")
+			return
+		}
+		log.Printf("current: %v", currentScore)
+
+		newScore := currentScore + change
+		log.Printf("new: %v", newScore)
+
+		err = redis.SetScore(newScore)
+		if err != nil {
+			tools.NewReply(c, msg, "接口错误，快喊陈🐷来修bug！")
+			return
+		}
+
+		newScoreString := fmt.Sprintf("当前陈🐷在小香🐷心目中的分数是：%v\n此次分数变化：%v\n新分数：%v，要继续努力哦～", currentScore, change, newScore)
+		tools.NewReply(c, msg, newScoreString)
+		return
 	}
 
-	tools.NewReply(c, msg, "未知指令")
+	tools.NewReply(c, msg, "小香🐷～这是什么新的指令嘛？快喊陈🐷来开发！开发完之后我就认识啦！")
 }
